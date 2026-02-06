900560109f5ef9e9301c5bbb43dcc039d18c6c4a
Dadminete <empresatecnologicadeleste@gmail.com>
2026-02-06 12:00:11 -0400

    feat: Remove database inspection scripts and generated schema, and update Next.js development configuration to allow dev origins.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    allowedDevOrigins: ['172.16.0.25', 'localhost:3000']
+  },
   images: {
     remotePatterns: [
       {
