ab1e73d6a302a70f5e0e46bd8a7d74427ebaeba2
creacionestecnologicadeleste-design <creacionestecnologicadeleste@gmail.com>
2026-04-10 12:40:41 -0400

    loginproblema2

--- .tmp-verify-db.js
+++
@@ -1,9 +0,0 @@
-const { Client } = require("pg");
-(async () => {
-  const connectionString = process.env.LOCAL_DATABASE_URL || process.env.DATABASE_URL;
-  const c = new Client({ connectionString });
-  await c.connect();
-  const count = await c.query("select count(*)::int as total from usuarios");
-  console.log("usuarios disponibles para DBSISMOVIL:", count.rows[0].total);
-  await c.end();
-})().catch(e => { console.error(e); process.exit(1); });
